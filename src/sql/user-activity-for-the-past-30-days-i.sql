8a045f9c25d11a17e400b4ca46da94714fbb21af
abdullah <abdullahelagami55@gmail.com>
2024-12-11 21:42:41 +0200

    SQL

---
+++ src/sql/user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,44 @@
+Input:
+Activity table:
++---------+------------+---------------+---------------+
+| user_id | session_id | activity_date | activity_type |
++---------+------------+---------------+---------------+
+| 1       | 1          | 2019-07-20    | open_session  |
+| 1       | 1          | 2019-07-20    | scroll_down   |
+| 1       | 1          | 2019-07-20    | end_session   |
+| 2       | 4          | 2019-07-20    | open_session  |
+| 2       | 4          | 2019-07-21    | send_message  |
+| 2       | 4          | 2019-07-21    | end_session   |
+| 3       | 2          | 2019-07-21    | open_session  |
+| 3       | 2          | 2019-07-21    | send_message  |
+| 3       | 2          | 2019-07-21    | end_session   |
+| 4       | 3          | 2019-06-25    | open_session  |
+| 4       | 3          | 2019-06-25    | end_session   |
++---------+------------+---------------+---------------+
+Output:
++------------+--------------+
+| day        | active_users |
++------------+--------------+
+| 2019-07-20 | 2            |
+| 2019-07-21 | 2            |
++------------+--------------+
+Explanation: Note that we do not care about days with zero active users.
+
+
+SELECT
+    sb.activity_date as day,
+    count(*) as active_users
+FROM
+    (
+    SELECT
+        a1.activity_date, a1.user_id, a1.activity_type
+    FROM
+        activity a1
+    WHERE
+       a1.activity_date > '2019-06-27' AND a1.activity_date <= '2019-07-27'
+    GROUP BY
+        a1.user_id, a1.activity_date
+    ) as sb
+
+GROUP BY
+    sb.activity_date
