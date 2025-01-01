be33840057c871f09ce60d981f91a7e7ab619d78
abdullah <abdullahelagami55@gmail.com>
2025-01-01 22:38:20 +0200

    sql: 1193. Monthly Transactions I

--- src/sql/monthly-transactions.sql
+++ src/sql/monthly-transactions.sql
@@ -2,23 +2,39 @@ SELECT
     DATE_FORMAT(t.trans_date, '%Y-%m') AS month,
     t.country,
     count(*) AS trans_count,
-    COALESCE(approved_count, 0) as approved_count,
+    COALESCE(approved_count, 0) AS approved_count,
     sum(amount) AS trans_total_amount,
-    COALESCE(sb.approved_total_amount, 0) as approved_total_amount
+    COALESCE(sb.approved_total_amount, 0) AS approved_total_amount
 
 FROM
     transactions t
     LEFT JOIN
-    (
-    SELECT country, count(*) as approved_count, sum(amount) as approved_total_amount, DATE_FORMAT(trans_date, '%Y-%m') AS month
-    FROM
-    transactions
-    WHERE
-    state = 'approved'
-    group by
-    month, country
-    ) as sb
+        (
+        SELECT country, count (*) AS approved_count, sum (amount) AS approved_total_amount, DATE_FORMAT(trans_date, '%Y-%m') AS month
+        FROM
+            transactions
+        WHERE
+            state = 'approved'
+        GROUP BY
+            month, country
+        ) as sb
 ON
     DATE_FORMAT(t.trans_date, '%Y-%m') = sb.month AND (t.country = sb.country OR t.country is null AND sb.country is null)
 GROUP BY
     DATE_FORMAT(t.trans_date, '%Y-%m'), t.country
+
+
+-- Simple answer :)
+
+
+SELECT
+    LEFT(trans_date, 7) AS month,
+    country,
+    COUNT(id) AS trans_count,
+    SUM(state = 'approved') AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM((state = 'approved') * amount) AS approved_total_amount
+FROM
+    Transactions
+GROUP BY
+    month, country;
