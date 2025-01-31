4bc9135774f9d0ea58085c9ed488e05d88677f5e
abdullah <abdullahelagami55@gmail.com>
2025-01-31 13:44:01 +0200

    cracking-coding-interview: LinkedList: Remove Duplicates

--- src/arrays/RemoveDuplicates.java
+++ src/arrays/RemoveDuplicates.java
@@ -2,25 +2,27 @@ package arrays;
 
 public class RemoveDuplicates {
     public int removeDuplicates(int[] nums) {
-        int p = 0, c = 1, i = 1;
-
-        for (i = 1; i < nums.length; i++) {
-            while (c < nums.length && nums[p] == nums[c]) {
-                c++;
-            }
-            if (c >= nums.length) {
+        int idx, i = 0, j = 0;
+        for (idx = 0; idx < nums.length; idx++) {
+            if (j >= nums.length) {
                 break;
             }
-            nums[i] = nums[c];
-            p = c;
-            c++;
+            while (j + 1 < nums.length && nums[j] == nums[j + 1]) {
+                j++;
+            }
+            j++;
+            nums[idx] = nums[i];
+            if (j - i > 1) {
+                nums[idx + 1] = nums[i];
+                idx++;
+            }
+            i = j;
         }
-        return i;
+        return idx;
     }
 
     public static void main(String[] args) {
         RemoveDuplicates removeDuplicates = new RemoveDuplicates();
-        System.out.println(removeDuplicates.removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4
-}));
+        System.out.println(removeDuplicates.removeDuplicates(new int[]{1,1,1,2,2,3}));
     }
 }
